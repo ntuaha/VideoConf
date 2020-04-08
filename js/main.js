a22612d5910ed5ba2991f508e36fe24cfa6d89a6
aha <ntuaha@gmail.com>
2020-04-08 23:35:34 +0800

    測試喔

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 const domain = 'meet.jit.si';
 const room_name = 'aha';
 $(function(){
+    // https://github.com/jitsi/jitsi-meet/blob/master/doc/api.md
+    $('#enter').click(function(){
+        let room =  $("#room").val()
+        let nickname = $("#name").val()
+        $('body').empty()
+        $('<div id="meet"></div>').appendTo("body")
+        console.log()
+        const options = {
+            roomName: room,
+            width: $(window).width(),
+            height: $(window).height(),
+            parentNode: document.querySelector('#meet'),
+            noSsl: false,
+            userInfo: {
+                //email: email,
+                displayName: nickname
+            }
+        };
+        
+        const api = new JitsiMeetExternalAPI(domain, options);
+        api.executeCommand('subject', room);
+        api.executeCommand('displayName', nickname);
+        console.log('go')
 
-    const options = {
-        roomName: room_name,
-        width: 700,
-        height: 700,
-        parentNode: document.querySelector('#meet'),
-        noSsl: false,
-        userInfo: {
-            email: 'ntuaha@gmail.com'
-        }
-    };
+    })
     
-    const api = new JitsiMeetExternalAPI(domain, options);
-    console.log('go')
 })
